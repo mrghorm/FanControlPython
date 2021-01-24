bd9bc643614dd82a450e1fb1829fbcfddbe3be7d
mrghorm <mrghorm@gmail.com>
2021-01-24 12:23:18 -0700

    Added comments and added test for CPUs

--- PyFanControl.py
+++ PyFanControl.py
@@ -8,34 +8,6 @@ from CPU import CPU
 from Fan import read_single_line_file
 from Fan import overwrite_file
 
-# ### FUNCTIONS ###
-#
-# # Read single line from file
-# def read_single_line_file(f):
-#     with open(f, 'r') as file_open:
-#         line = file_open.readline()
-#         line = line.rstrip()
-#
-#         return line
-#
-# # TRUNCATE & Write File
-# def overwrite_file(f, message):
-#
-#     with open(f, 'w') as file_open:
-#         file_open.truncate()
-#         file_open.write(message)
-
-
-def get_highest_temp_from_cpu(cpu_list):
-    highest = 0
-    for i in cpu_list:
-        x = int(i.get_current_temp())
-        if x > highest:
-            highest = x
-
-    return highest
-
-
 ######  DEFINITIONS  ######
 
 coretemp0_parent_dir = "/sys/devices/platform/coretemp.0/hwmon/hwmon1/"
@@ -45,29 +17,7 @@ fan_parent_dir = "/sys/devices/platform/applesmc.768/"
 
 ###########################
 
-
-
-#cpu_a_temps = []
-#for i in range(2,8):
-#    if(i==5):
-#        x = 10
-#    elif(i==6):
-#        x = 11
-#    elif(i== 7):
-#        x = 12
-#    else:
-#        x = i
-#
-#    cpu_a_temps.append(Temp("cpua_{}".format(x), coretemp0_parent_dir, "temp{}".format(x)))
-#
-#
-#for i in cpu_a_temps:
-#    Temp.update_all_current_temps()
-#    print(i.name)
-#    print(i.get_current_temp())
-#
-#print("Highest: {0}".format(get_highest_temp_from_cpu(cpu_a_temps)))
-
+# Initialize Fans
 fan_boosta = Fan("BOOSTA", fan_parent_dir, "fan5")
 fan_boostb = Fan("BOOSTB", fan_parent_dir, "fan6")
 fan_intake = Fan("INTAKE", fan_parent_dir, "fan4")
@@ -75,24 +25,45 @@ fan_exhaust = Fan("EXHAUST", fan_parent_dir, "fan3")
 fan_ps = Fan("PS", fan_parent_dir, "fan2")
 fan_pci = Fan("PCI", fan_parent_dir, "fan1")
 
+# Set fans to manual control
 fan_boosta.set_manual(1)
 fan_boostb.set_manual(1)
+fan_ps.set_manual(1)
+fan_pci.set_manual(1)
+fan_intake.set_manual(1)
+fan_exhaust.set_manual(1)
 
+#### Initialize Sensors ####
+# CPU 1
 cpu1 = CPU("cpu1", coretemp0_parent_dir, "temp")
-cpu2 = CPU("cpu2", coretemp1_parent_dir, "temp")
 cpu1.set_max(45)
 cpu1.set_min(30)
+
+# CPU 2
+cpu2 = CPU("cpu2", coretemp1_parent_dir, "temp")
 cpu2.set_max(45)
 cpu2.set_min(30)
 
+# Power Supply
 ps_temp = Temp("Tp0C", fan_parent_dir, "temp72")
+ps_temp.set_min_temp(30)
+ps_temp.set_max_temp(50)
+
+# PCI Bay
 pci_temp = Temp("Te1P", fan_parent_dir, "temp57")
-southbridge_diode_temp = Temp("TN0D", fan_parent_dir, "temp53")
-southbridge_heatsink_temp = Temp("TN0H", fan_parent_dir, "temp54")
+pci_temp.set_min_temp(30)
+pci_temp.set_max_temp(50)
 
+# Northbridge Diode
+northbridge_diode_temp = Temp("TN0D", fan_parent_dir, "temp53")
+northbridge_diode_temp.set_min_temp(60)
+northbridge_diode_temp.set_max_temp(90)
 
-southbridge_diode_temp.set_min_temp(60)
-southbridge_diode_temp.set_max_temp(90)
+# Northbridge Heatsink
+northbridge_heatsink_temp = Temp("TN0H", fan_parent_dir, "temp54")
+northbridge_heatsink_temp.set_min_temp(50)
+northbridge_heatsink_temp.set_max_temp(80)
+
 
 
 # Main Loop
@@ -102,14 +73,26 @@ while True:
     Fan.update_current_rpms()
     Temp.update_all_current_temps()
 
-    sbd_p = southbridge_diode_temp.get_current_percentage()
-    fan_intake.request_set_percentage(sbd_p)
-    fan_exhaust.request_set_percentage(sbd_p)
-    fan_boosta.request_set_percentage(sbd_p)
-    fan_boostb.request_set_percentage(sbd_p)
+    # Northbridge Diode Logic
+    nbd_p = northbridge_diode_temp.get_current_percentage()
+    fan_intake.request_set_percentage(nbd_p)
+    fan_exhaust.request_set_percentage(nbd_p)
+    fan_boosta.request_set_percentage(nbd_p)
+    fan_boostb.request_set_percentage(nbd_p)
 
-    temp_percent = cpu1.get_current_percentage_highest()
+    # CPU 1 Logic
+    cpu1_tp = cpu1.get_current_percentage_highest()
+    fan_intake.request_set_percentage(cpu1_tp)
+    fan_exhaust.request_set_percentage(cpu1_tp)
+    fan_boosta.request_set_percentage(cpu1_tp)
 
+    # CPU 2 Logic
+    cpu2_tp = cpu2.get_current_percentage_highest()
+    fan_intake.request_set_percentage(cpu2_tp)
+    fan_exhaust.request_set_percentage(cpu2_tp)
+    fan_boostb.request_set_percentage(cpu2_tp)
+
+    # Write requested fan RPM to driver files
     Fan.write_request()
 
     frame = '''
@@ -146,8 +129,8 @@ BOOSTB:  {15}
         ,cpu2.get_temps()
         ,cpu2.get_highest_temp()
         ,cpu2.get_average_temp()
-        ,southbridge_diode_temp.get_current_temp()
-        ,southbridge_heatsink_temp.get_current_temp()
+        ,northbridge_diode_temp.get_current_temp()
+        ,northbridge_heatsink_temp.get_current_temp()
         ,ps_temp.get_current_temp()
         ,pci_temp.get_current_temp()
         ,fan_ps.get_current_rpm()
