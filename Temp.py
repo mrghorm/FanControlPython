5bf1e6492c8abb80d1106d96a0d083decbbca669
mrghorm <mrghorm@gmail.com>
2021-01-24 15:14:18 -0700

    Added more error checking to file read functions

--- Temp.py
+++ Temp.py
@@ -48,7 +48,30 @@ class Temp:
 
     # Returns current temperature found from sensors input file
     def read_current_temp(self):
-        return round(int(read_single_line_file(self.input_file)) / 1000)
+        #return round(int(read_single_line_file(self.input_file)) / 1000)
+
+        i = 0
+
+        # Try to cast the value to an int, divide by 1000, and round to nearest 1
+        try:
+            i = round(int(read_single_line_file(self.input_file)) / 1000)
+
+        # Catch for integer casting error, attempt to continue.  Return last good value in this event
+        except ValueError as e:
+            print("Value Error casting to an integer, file may be empty or may have received an input/output error.")
+            print("Error:  {0}\nLocation:  {1}".format(str(e), str(self.input_file)))
+            print("Using last known good temperature value")
+            return self.temp_current
+
+        # Catch for other errors, attempt to continue, return last known-good value
+        except Exception as e:
+            print(str(e))
+            print("#### Attempting to continue...\n\n")
+            return self.temp_current
+
+        # Else return casted value if try is a success
+        else:
+            return i
 
     # Read and update current temperature from input file
     def update_current_temp(self):
@@ -89,14 +112,44 @@ class Temp:
 # Read single line from file
 def read_single_line_file(f):
     with open(f, 'r') as file_open:
-        line = file_open.readline()
-        line = line.rstrip()
 
-        return line
+        line = ""
+
+        # Try to read the line from the file
+        try:
+            line = file_open.readline()
+
+        # Catch for OSError Errno 5:
+        # Sometimes, the applesmc driver fails to read from the applesmc.  In this case, reading the file results in
+        # OSError errno 5.  The file may be accessible after waiting and attempting to read the file again.
+        except OSError as e:
+            if e.errno == 5:
+                print("Error reading {0}:  Input/output error, retry next cycle".format(f))
+                return "-1"
+            else:
+                print("Error reading {0}:  Errno {1}".format(f, e.errno))
+                return "-1"
+
+        # General exception catch.  Attempt to continue even if file is not readable.
+        except Exception as e:
+            print(str(e))
+            print("#### Attempting to continue...\n\n")
+            return "-1"
+
+        # If try statement runs successfully, continue as normal
+        else:
+
+            # Pull newlines & spaces out of output
+            line = line.rstrip()
+            return line
 
 # TRUNCATE & Write File
 def overwrite_file(f, message):
 
     with open(f, 'w') as file_open:
+
+        # Truncate removes the current contents of the file
         file_open.truncate()
+
+        # Write value to file
         file_open.write(message)
